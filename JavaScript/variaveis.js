64821dd9a7f204b2704aa8cae3f54339bb8cc499
João Pedro Miranda Santos <jpmsantosti@gmail.com>
2026-02-23 08:48:10 -0300

    Add files via upload

---
+++ JavaScript/variaveis.js
@@ -0,0 +1,19 @@
+var nomedavariavel = "banana";
+var outravariavel = "maçã";
+var variavelvarialistica = 44;
+var variavelgeracionalparapessoasgeracionais = false;
+var variavelvazia  ;
+
+console.log (nomedavariavel)
+console.log (outravariavel)
+console.log (variavelvarialistica)
+console.log (variavelgeracionalparapessoasgeracionais)
+console.log (variavelvazia)
+
+var a = 14;
+var b = 42;
+var tr = a == 14;
+var fl = a == b;
+var int1 = tr + 1
+var int2 = fl +1
+console.log(int1, int2)
