38c977252583e189e9b691eb18ad98d4e17ccadb
João Pedro Miranda Santos <jpmsantosti@gmail.com>
2026-03-02 08:28:01 -0300

    Add files via upload

---
+++ JavaScript/Aula3/Execicios.js
@@ -0,0 +1,37 @@
+console.log("Exercicio A")
+var a = 10
+var b = 20
+console.log(b)
+b = 5
+console.log(a,b)
+console.log("Exercicio B")
+a = 30
+b = 20
+c = a + b
+console.log(c)
+b = 10
+console.log(b, c)
+c = a + b
+console.log(a, b, c)
+console.log("Exercicio C")
+a= 10
+b = 20
+c = a
+b = c
+a = b
+console.log(a, b, c)
+console.log("Execicio D")
+a = 10
+b = a + 1
+a = b = 1
+b = a + 1
+console.log(a)
+a = b + 1
+console.log(a, b)
+
+console.log("Exercicio 2")
+var nota1 = 10
+var nota2 = 9
+var nota3 = 7
+var mediafinal = nota1 * 2 + nota2 * 3 + nota3 * 5
+console.log(mediafinal / 10)
